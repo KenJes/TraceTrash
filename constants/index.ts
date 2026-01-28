af8429ce954648b0439cb67977097cd4ad848027
kenneth <kenneth_wwe@live.com.mx>
2026-01-28 09:24:48 -0600

    Limpiar rama docs - eliminar todos los archivos de la app React Native

--- constants/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Exportaciones centralizadas de constantes
- * Simplifica imports en toda la aplicación
- */
-
-export * from "./colors";
-export * from "./map-constants";
-export * from "./theme";
-
